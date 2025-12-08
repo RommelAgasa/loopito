d384689c1ef543f7bda1ebe688401a6445796119
_DevNote <rommelagasa102@gmail.com>
2025-12-08 12:56:03 +0800

    add variable in import

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { getMembers, addMember, updateMember, deleteMember } from '../controllers/MemberController.js';
+import { 
+    getMembers, 
+    addMember, 
+    updateMember, 
+    deleteMember, 
+} from '../controllers/MemberController.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
