32b9ce36f64d850f71927532bf32c386f6bdc7ad
_DevNote <rommelagasa102@gmail.com>
2025-12-08 09:19:28 +0800

    fix api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,8 +12,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': {
           target: env.VITE_API_URL || 'http://localhost:5000/',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true
         },
       },
     },
